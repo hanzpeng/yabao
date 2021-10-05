949e6255a2fdb50bca81a01d79ffa8729ce7f92e
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-04 23:33:16 -0700

    update

--- src/app/demo/checkbox/checkbox.component.ts
+++ src/app/demo/checkbox/checkbox.component.ts
@@ -1,4 +1,7 @@
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/core/models/student';
 
 @Component({
   selector: 'app-checkbox',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  loaded = new BehaviorSubject<boolean>(false);
+  students: Student[] = [];
+  selectedStudents: Student[] = [];
 
   ngOnInit(): void {
+    let tempSubs = this.http.get<Student[]>("assets/students.json").subscribe(
+      (res: Student[]) => {
+        this.students = res;
+        tempSubs.unsubscribe();
+        this.loaded.next(true);
+      },
+      error => {
+        alert("failed getting students");
+        tempSubs.unsubscribe();
+      }
+    )
+  }
+
+  toggleSelected(student: Student) {
+    let itemFound = false;
+    this.selectedStudents.forEach((member, index) => {
+      if (member == student) {
+        itemFound = true;
+        this.selectedStudents.splice(index, 1);
+      }
+    });
+    if (!itemFound) {
+      this.selectedStudents.push(student);
+    }
+  }
+
+  isSelected(student:Student): boolean{
+    return this.selectedStudents.some(member =>{
+      return member === student;
+    });
   }
 
 }
