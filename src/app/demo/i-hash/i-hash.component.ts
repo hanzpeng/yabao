a91d078ca4449d3e6f0d0f0caedb1aec849567a5
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-06 15:59:00 -0700

    update

--- src/app/demo/i-hash/i-hash.component.ts
+++ src/app/demo/i-hash/i-hash.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IHash<TValue> {
+  [index: string]: TValue;
+}
+
+export class ErrorResult {
+  message: string;
+  messageDetail: string;
+}
+
 @Component({
   selector: 'app-i-hash',
   templateUrl: './i-hash.component.html',
@@ -8,8 +17,20 @@ import { Component, OnInit } from '@angular/core';
 export class IHashComponent implements OnInit {
 
   constructor() { }
+  failedResults: IHash<ErrorResult> = {};
 
   ngOnInit(): void {
-  }
+    this.failedResults["one"] = <ErrorResult>{
+      message:"message1",
+      messageDetail:"messageDetail1"
+    };
+    this.failedResults["two"] = <ErrorResult>{
+      message:"message2",
+      messageDetail:"messageDetail2"
+    };
 
+    for(let key in this.failedResults){
+      console.log(this.failedResults[key].message + " "+ this.failedResults[key].messageDetail );
+    }
+  }
 }
