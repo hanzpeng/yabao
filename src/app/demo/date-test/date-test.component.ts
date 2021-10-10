d40785fa3511d1881a8105474c2576c4192173a1
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-10 15:46:45 -0700

    DateTest

--- src/app/demo/date-test/date-test.component.ts
+++ src/app/demo/date-test/date-test.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-date-test',
   templateUrl: './date-test.component.html',
   styleUrls: ['./date-test.component.scss']
 })
-export class DateTestComponent implements OnInit {
+export class DateTestComponent implements AfterViewInit {
+  @ViewChild("grid") grid: ElementRef;
+  ngAfterViewInit() {
+    this.addRow(
+      "new Date()",
+      new Date()
+    );
 
-  constructor() { }
+    this.addRow(
+      "((new Date()).valueOf() + 24 * 3600 * 1000)",
+      new Date((new Date()).valueOf() + 24 * 3600 * 1000)
+    );
 
-  ngOnInit(): void {
+    this.addRow(
+      "new Date('2019-08-21T11:30:00-06:00')",
+      new Date("2019-08-21T11:30:00-06:00")
+    );
+
+    this.addRow(
+      "new Date(2019, 0, 2, 11, 30, 25, 10)",
+      new Date(2019, 0, 2, 11, 30, 25, 10)
+    );
   }
 
+  addRow(label: string, value: any) {
+    this.grid.nativeElement.innerHTML += `<div>${label}</div><div>${value}</div>`
+  }
 }
