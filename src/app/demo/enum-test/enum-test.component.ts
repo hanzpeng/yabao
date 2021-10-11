440f295b4c9fcc1a3ccac6691e7d20ab0b3bb3d9
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-11 15:30:45 -0700

    update

--- src/app/demo/enum-test/enum-test.component.ts
+++ src/app/demo/enum-test/enum-test.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-enum-test',
   templateUrl: './enum-test.component.html',
   styleUrls: ['./enum-test.component.scss']
 })
-export class EnumTestComponent implements OnInit {
+export class EnumTestComponent implements OnInit, AfterViewInit {
 
   constructor() { }
-
+  @ViewChild("grid") grid: ElementRef;
   ngOnInit(): void {
+  }
+  ngAfterViewInit() {
+    this.displayContent();
+  }
+  addRow(label: string, value: any) {
+    this.grid.nativeElement.innerHTML += `<div>${label}</div><div>${value}</div>`
+  }
+
+  displayContent() {
+      this.addRow("expression","value");
   }
 
 }
