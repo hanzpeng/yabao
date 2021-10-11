92db1224aa7678c6e1ff70681463313b13bb6a53
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-11 15:59:54 -0700

    udpdate

--- src/app/demo/enum-test/enum-test.component.ts
+++ src/app/demo/enum-test/enum-test.component.ts
@@ -1,5 +1,18 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
+export enum RequestType {
+  None = 0,
+  Tour = 1,
+  Temporary = 2,
+  Permanent = 3,
+  Visit = 4,
+  Emergency = 5,
+  Governmental = 6,
+  RestrictedAccess = 7,
+  SpecialPermissions = 8,
+  TerminatePersons = 9
+}
+
 @Component({
   selector: 'app-enum-test',
   templateUrl: './enum-test.component.html',
@@ -11,15 +24,33 @@ export class EnumTestComponent implements OnInit, AfterViewInit {
   @ViewChild("grid") grid: ElementRef;
   ngOnInit(): void {
   }
+
   ngAfterViewInit() {
     this.displayContent();
   }
+
   addRow(label: string, value: any) {
     this.grid.nativeElement.innerHTML += `<div>${label}</div><div>${value}</div>`
   }
 
+  toRequestType(x: any): RequestType {
+    if(typeof x ==="string"){
+      return RequestType[x];
+    }else if(typeof x ==="number"){
+      return <RequestType>x;
+    }else{
+      return RequestType.None;
+    }
+  }
+
   displayContent() {
-      this.addRow("expression","value");
+      this.addRow("RequestType.Permanent",RequestType.Permanent);
+      this.addRow('RequestType["Permanent"]',RequestType["Permanent"]);
+      this.addRow('RequestType["Permanent"]',this.toRequestType("Permanent"));
+      this.addRow('RequestType["Permanent"]',this.toRequestType(RequestType.Permanent));
+      this.addRow('RequestType["Permanent"]',this.toRequestType(14));
+      this.addRow('RequestType["Permanent"]',this.toRequestType({x:3}));
+      this.addRow('RequestType["Permanent"]',this.toRequestType("permanent"));
   }
 
 }
