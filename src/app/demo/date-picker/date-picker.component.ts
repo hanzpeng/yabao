b09c7259fb43493a7af2c770361d739493544128
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-12 15:26:35 -0700

    udate

--- src/app/demo/date-picker/date-picker.component.ts
+++ src/app/demo/date-picker/date-picker.component.ts
@@ -1,5 +1,3 @@
-//https://stackblitz.com/edit/demo-ngx-mat-datetime-picker?file=src%2Fapp%2Fapp.component.html
-//https://www.npmjs.com/package/@angular-material-components/datetime-picker
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {MatAccordion} from '@angular/material/expansion';
 import * as dayjs from 'dayjs';
@@ -7,46 +5,11 @@ import * as moment from 'moment'
 import * as utc from 'dayjs/plugin/utc';
 import * as timezone from 'dayjs/plugin/timezone'
 import { DayjsTimezone } from 'dayjs';import { FormControl } from '@angular/forms';
-// import * as moment from 'moment';
-// import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, ThemePalette } from '@angular/material/core';
-// import {MomentDateAdapter} from '@angular/material-moment-adapter';
-// import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material';
-
-// Depending on whether rollup is used, moment needs to be imported differently.
-// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
-// syntax. However, rollup creates a synthetic default module and we thus need to import it using
-// the `default as` syntax.
-//import * as _moment from 'moment';
-// tslint:disable-next-line:no-duplicate-imports
-//import {default as _rollupMoment} from 'moment';
-
-//const moment = _rollupMoment || _moment;
-
-// See the Moment.js docs for the meaning of these formats:
-// https://momentjs.com/docs/#/displaying/format/
-// export const MY_FORMATS = {
-//   parse: {
-//     dateInput: 'LL',
-//   },
-//   display: {
-//     dateInput: 'YYYY-MM-DD, HH:MM',
-//     monthYearLabel: 'YYYY',
-//     dateA11yLabel: 'LL',
-//     monthYearA11yLabel: 'YYYY',
-//   },
-// };
 
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.scss'],
-  // providers: [
-  //   // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
-  //   // application's root module. We provide it at the component level here, due to limitations of
-  //   // our example generation script.
-  //   // {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-  //   // {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
-  // ],
+  styleUrls: ['./date-picker.component.scss']
 })
 export class DatePickerComponent implements OnInit {
   constructor() {}
