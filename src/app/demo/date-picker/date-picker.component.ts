ab6f1e921a22a603f9ebf891d7a10ad37083623b
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-12 15:54:48 -0700

    update

--- src/app/demo/date-picker/date-picker.component.ts
+++ src/app/demo/date-picker/date-picker.component.ts
@@ -1,10 +1,17 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {MatAccordion} from '@angular/material/expansion';
+import { FormControl } from '@angular/forms';
+
 import * as dayjs from 'dayjs';
-import * as moment from 'moment'
 import * as utc from 'dayjs/plugin/utc';
+import * as minMax from 'dayjs/plugin/minMax';
 import * as timezone from 'dayjs/plugin/timezone'
-import { DayjsTimezone } from 'dayjs';import { FormControl } from '@angular/forms';
+import { DayjsTimezone } from 'dayjs';
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(minMax);
+
+
 
 @Component({
   selector: 'app-date-picker',
@@ -46,5 +53,21 @@ export class DatePickerComponent implements OnInit {
   testa(){
     this.adda("localDate",this.date.toString());
     this.adda("isoDate",this.date.toISOString());
+    let nextDay = dayjs(this.date).add(1, 'day').toDate();
+    this.adda("nextDay",nextDay);
+    let nextDay2 = dayjs(this.date).add(2, 'day').toDate();
+    this.adda("nextDay2",nextDay2);
+
+    let startDates = [];
+    startDates.push(dayjs(this.date));
+    startDates.push(dayjs(nextDay));
+    startDates.push(dayjs(nextDay2));
+    let startedAtMin = dayjs.min(startDates);
+    this.adda("startedAtMin",startedAtMin.toDate());
+    let startedAtMax = dayjs.max(startDates).toDate()
+    this.adda("startedAtMin",startedAtMax);
+
+
+
   }
 }
