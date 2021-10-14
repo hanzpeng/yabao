3d406831669e04e577708c91d6c569850cd5eeca
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-13 17:38:02 -0700

    update

--- src/app/demo/date-time-picker-test/date-time-picker-test.component.ts
+++ src/app/demo/date-time-picker-test/date-time-picker-test.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-date-time-picker-test',
   templateUrl: './date-time-picker-test.component.html',
   styleUrls: ['./date-time-picker-test.component.scss']
 })
 export class DateTimePickerTestComponent implements OnInit {
-
   constructor() { }
+  currentDateCtrl = new FormControl();
+  currentDateValue: Date;
 
   ngOnInit(): void {
+    let now = new Date();
+    this.currentDateValue = now;
+    this.currentDateCtrl.setValue(now);
   }
 
+  dateTimeChange(newDate: Date) {
+    this.currentDateValue = newDate;
+  }
 }
