03c8999a5660b6db9736ca27f31fa7462e618ebb
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-19 14:07:01 -0700

    update typescript test

--- src/app/demo/type-script/type-script.component.ts
+++ src/app/demo/type-script/type-script.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-type-script',
   templateUrl: './type-script.component.html',
   styleUrls: ['./type-script.component.scss']
 })
-export class TypeScriptComponent implements OnInit {
-
+export class TypeScriptComponent implements OnInit, AfterViewInit {
   constructor() { }
-
   ngOnInit(): void {
+  }
+  ngAfterViewInit():void{
+    this.tests();
+  }
+  @ViewChild("grid") grid: ElementRef;
+  addRow(label: string, value: any) {
+    this.grid.nativeElement.innerHTML += `<div>${label}</div><div>${value}</div>`
+  }
+  tests() {
+    this.addRow("expression", "value");
   }
 
 }
