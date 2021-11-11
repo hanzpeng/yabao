04d5719a60286b8fecc2d8b0151b92e6ce08c36b
Hanz Zhang <hanzzhan@microsoft.com>
2021-11-11 15:31:42 -0800

    update

--- src/app/demo/type-script/type-script.component.ts
+++ src/app/demo/type-script/type-script.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
-enum Color{
+enum Color {
   Red,
   Green,
   Blue,
@@ -16,32 +16,81 @@ export class TypeScriptComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
   }
   ngAfterViewInit(): void {
-    this.testsEnumConversion();
+    // this.testsEnumConversion();
+    // this.testAsignWithOr();
+    // this.testMapDefault();
+    this.testMapForEach();
   }
   @ViewChild("grid") grid: ElementRef;
-  addRow(label: string, value: any) {
+  add(label: string, value?: any) {
+    if(!value){
+      value = "";
+    }
     this.grid.nativeElement.innerHTML += `<div>${label}</div><div>${value}</div>`
+  }
+
+  testMapDefault() {
+    const map = new Map<string, string>();
+    map.set("a", "AAA");
+    map.set("b", "BB");
+    this.add("a", map.get("a"));
+    this.add("c", map.get("c"));
+    this.add("d", null);
+    this.add("c", !!map.get("c"));
+    this.add("d", !!map.get("d"));
+    map.forEach((value, key) => {
+      this.add(key, value);
+    })
+  }
+
+  testMapForEach() {
+    const map = new Map<string, string>();
+    map.set("a", "AAA");
+    map.set("b", "BB");
+    map.set("c", null);
+    map.forEach((value, key) => {
+      this.add(key, value);
+    });
+    map.forEach((value) => {
+      if (!!value) {
+        this.add("", value);
+      }
+    });
+    map.forEach((vaule, key) => {
+      this.add("key1", key);
+    })
+
+    map.size;
+    this.add("map.size", map.size)
+
+    for (let id of map) {
+      this.add(id.toString());
+    }
+
+    map.forEach((vaule: string, key:string) => {
+      this.add(key);
+    });
   }
 
   testsEnumConversion() {
     const red = Color.Red;
-    this.addRow("Color.Red",Color.Red);
-    this.addRow("Color.Green",Color.Green);
-    this.addRow("Color.Blue",Color.Blue);
-    this.addRow("Color['Red']", Color['Red']);
-    this.addRow("Color['Blue']", Color['Blue']);
-    this.addRow("Color[Color.Blue]", Color[Color.Blue]);
-    this.addRow("Color[2]", Color[2]);
-    this.addRow("Color.Blue", Color.Blue);
+    this.add("Color.Red", Color.Red);
+    this.add("Color.Green", Color.Green);
+    this.add("Color.Blue", Color.Blue);
+    this.add("Color['Red']", Color['Red']);
+    this.add("Color['Blue']", Color['Blue']);
+    this.add("Color[Color.Blue]", Color[Color.Blue]);
+    this.add("Color[2]", Color[2]);
+    this.add("Color.Blue", Color.Blue);
   }
 
-  testAsignWithOr(){
+  testAsignWithOr() {
     //Test ||
-    this.addRow("||", "");
-    this.getSafetyOrientationStatus("MWH","hanz", new Date());
-    this.getSafetyOrientationStatus("MWH","hanz");
+    this.add("||", "");
+    this.getSafetyOrientationStatus("MWH", "hanz", new Date());
+    this.getSafetyOrientationStatus("MWH", "hanz");
     let name;
-    this.getSafetyOrientationStatus("MWH",name);
+    this.getSafetyOrientationStatus("MWH", name);
   }
 
   getSafetyOrientationStatus(datacenterId: string, userId: string, endedAt?: Date): void {
@@ -50,7 +99,7 @@ export class TypeScriptComponent implements OnInit, AfterViewInit {
       userId: userId || null,
       endedAt: endedAt || null
     };
-    this.addRow(`datacenterId: ${datacenterId}, userId: ${userId}, endedAt: ${ endedAt }`, JSON.stringify(searchQueryParams));
+    this.add(`datacenterId: ${datacenterId}, userId: ${userId}, endedAt: ${endedAt}`, JSON.stringify(searchQueryParams));
   }
 
 
